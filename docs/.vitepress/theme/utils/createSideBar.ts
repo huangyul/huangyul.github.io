d89bd9bf65b2f9f986f778912f7d0b0994f5837f
huangyul <1103221645@qq.com>
2024-11-20 15:28:44 +0800

    update: makefile basics

--- docs/.vitepress/theme/utils/createSideBar.ts
+++ docs/.vitepress/theme/utils/createSideBar.ts
@@ -2,10 +2,10 @@ export function createSideBarZH() {
 	return {
 		"/notes/": [
 			{
-				text: "Makefile基础",
+				text: "Makefile",
 				collapsed: false,
 				items: [
-					{ text: "01基础知识", link: "/notes/Makefile基础/01基础知识" },
+					{ text: "01基础知识", link: "/notes/Makefile/01基础知识" },
 				]
 			}
 		].map((item, i) => (!i ? item : { ...item, collapsed: true })),
