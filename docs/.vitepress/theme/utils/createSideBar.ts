27a70abbf6069b01919d6ff9d6b1bcb310b8e9e9
huangyul <1103221645@qq.com>
2024-11-27 15:52:54 +0800

    docs: update

--- docs/.vitepress/theme/utils/createSideBar.ts
+++ docs/.vitepress/theme/utils/createSideBar.ts
@@ -13,6 +13,7 @@ export function createSideBarZH() {
 				collapsed: false,
 				items: [
 					{ text: "01gin相关笔记", link: "/notes/GO项目实战笔记/01gin相关笔记" },
+					{ text: "02性能相关", link: "/notes/GO项目实战笔记/02性能相关" },
 				]
 			}
 		].map((item, i) => (!i ? item : { ...item, collapsed: true })),
